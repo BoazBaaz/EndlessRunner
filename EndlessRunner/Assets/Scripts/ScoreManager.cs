15a35b40078183e819bfd206fbca20b04e940e8e
LucaMouissie <mouissiel@hotmail.com>
2021-02-09 16:37:16 +0100

    Scene Merge
    Added Platforms to main scene;
    Added Score to main scene;
    Added Main Menu Scene.

--- EndlessRunner/Assets/Scripts/ScoreManager.cs
+++ EndlessRunner/Assets/Scripts/ScoreManager.cs
@@ -10,12 +10,16 @@ public class ScoreManager : MonoBehaviour
 
     private void Start()
     {
+        // Sets the given variable to the given string plus variable, and starts the coroutine.
+
         ScoreTxt.text = string.Format("Score: {0}", scoreAmount);
         StartCoroutine("AddScore");
     }
 
     IEnumerator AddScore()
     {
+        // Repeatedly adds one score to the variable every one second.
+
         while (true)
         {
             for (int i = 0; i < 1; i++)
